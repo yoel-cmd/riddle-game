91ca77b2bd0d016b9fd3b3bc26c2e7324f3898a8
yoel ider <yoeli5746@gmail.com>
2025-06-26 11:58:35 +0300

    firs commit

---
+++ riddle-game/riddles/r1.js
@@ -0,0 +1,7 @@
+
+const r1={
+    id:1,
+    name:"Easy Math",
+    taskDescription:"What is 5+3 ?",
+    coorectAnswer:'8'
+}
---
+++ riddle-game/riddles/r2.js
@@ -0,0 +1,7 @@
+
+const r2={
+    id:1,
+    name:"Mediume Math",
+    taskDescription:"What is 7*9 ?",
+    coorectAnswer:'63'
+}
