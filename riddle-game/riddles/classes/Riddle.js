f2905eea850cec8a84bb9d1cfc1cbc42d2fafd9e
yoel ider <yoeli5746@gmail.com>
2025-06-29 17:34:01 +0300

    finish

--- riddle-game/riddles/classes/Riddle.js
+++ riddle-game/riddles/classes/Riddle.js
@@ -1,21 +1,27 @@
 import readlineSync from 'readline-sync';
-export default class Riddle{
-    constructor({id,name,taskDescription,corectAnswer}){
-        this.id=id;
-        this.name=name;
-        this.taskDescription=taskDescription;
-        this.corectAnswer=corectAnswer;
+
+/**
+ * Represents a single riddle question.
+ */
+export default class Riddle {
+    constructor({ id, name, taskDescription, corectAnswer }) {
+        this.id = id;
+        this.name = name;
+        this.taskDescription = taskDescription;
+        this.corectAnswer = corectAnswer;
     }
-    ask(){
-        console.log(this.taskDescription); 
-        let res=readlineSync.question('enter your answer')
-        while(this.corectAnswer!=res){
-            console.log(" Wrong answer"); 
-            console.log(this.taskDescription); 
-            res=readlineSync.question('enter your answer') 
+
+    /**
+     * Asks the riddle until the user gives the correct answer.
+     */
+    ask() {
+        console.log(this.taskDescription);
+        let res = readlineSync.question('enter your answer');
+        while (this.corectAnswer != res) {
+            console.log("Wrong answer");
+            console.log(this.taskDescription);
+            res = readlineSync.question('enter your answer');
         }
-        console.log("corect answer");
-        
+        console.log("correct answer");
     }
 }
-
