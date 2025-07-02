337e975a3f268b94a8011847dd85b600612e3401
yoel ider <yoeli5746@gmail.com>
2025-07-02 12:33:10 +0300

    finish

--- riddle-game/app.js
+++ riddle-game/app.js
@@ -1,25 +1,4 @@
-/**
- * Main script that runs the riddle game.
- * - Asks the user for their name.
- * - Loads riddles from an external source.
- * - Times each riddle solving attempt.
- * - Records results and shows the player's average time.
- */
+import { startGame } from "./riddles/game.js"
 
-import readlineSync from './node_modules/readline-sync/lib/readline-sync.js';
-import player from './classes/Player.js';
-import riddle from './classes/Riddle.js';
-import _import from './riddles/import.js';
 
-let namePlayer = readlineSync.question("enter your name");
-const person = new player(namePlayer);
-
-for (let i = 0; i < _import.length; i++) {
-    let starst = Date.now();
-    let riddles = new riddle(_import[i]);
-    riddles.ask();
-    person.recordTime(starst);
-}
-
-console.log(person);
-console.log(person.showStatus());
+startGame();
