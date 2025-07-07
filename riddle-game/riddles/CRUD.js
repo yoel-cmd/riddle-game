240cc83569664418f7d2f9133ac16d181e749485
yoel ider <yoeli5746@gmail.com>
2025-07-07 16:10:42 +0300

    finish CRUD

--- riddle-game/riddles/CRUD.js
+++ riddle-game/riddles/CRUD.js
@@ -1,31 +1,131 @@
 import { readFile, writeFile } from "node:fs/promises";
+import readlineSync from '../node_modules/readline-sync/lib/readline-sync.js';
 
-const path='../DB/RiddlesDB.txt';
+const pathRiddle='../DB/RiddlesDB.txt';
+const pathPlayer='../DB/PlayersDB.txt';
 
-//adding try,and async - Because every function is related to the other.
-async function creatRiddel(riddle) {
+
+async function creatRiddle() {
+    let nameRiddle=readlineSync.question('enter your name riddle')
+    let Description=readlineSync.question('enter your riddl')
+    let Answer=readlineSync.question('enter answer')
+    let riddle={
+        name:nameRiddle,
+        taskDescription:Description,
+        corectAnswer:Answer
+    }
+    return riddle;
+}
+
+async function creatPlayer() {
     try{
-    const data=await readFile(path,'utf-8');
+    let playerName=readlineSync.question('enter your name player')
+    let player={
+        name:playerName
+    }
+    return player;
+    }catch(err){
+        console.error('err',err.message)
+    }
+}
+
+
+async function Update(path){
+    try{
+    const data=await readFile(path,'utf8')
+    console.log(data);
     const arr=JSON.parse(data)
-    const riddleJson=JSON.parse(riddle)
-
-    let currentId = 0
-    arr.length === 0 ? currentId = 1 : currentId = arr[arr.length].id + 1
-    riddleJson.id = currentId;
-    arr.push(riddleJson);
+    const id=readlineSync.question("enrer id riddle")
+    const type=readlineSync.question("enrer line to Change")
+    const Change=readlineSync.question('enter Change')
+    for (let index = 0; index < arr.length; index++) {
+        if(arr[index].id==id){
+            arr[index][type]=Change;   
+            await writeFile(path,JSON.stringify(arr,null,2))        
+            return 'Update'
+        }  
+    }
+    console.log("id not found");
     
+    } catch(err){
+        console.error('err',err.message)
+    }
+}
 
+async function Delete(path){
+    try{
+    const data=await readFile(path,'utf8')
+    const arr=JSON.parse(data)
+    const id=readlineSync.question("enrer id riddle")
+    for (let index = 0; index < arr.length; index++) {
+        if(arr[index].id==id){
+            arr.splice(index,1)
+            break
+        }  
+    }
     await writeFile(path,JSON.stringify(arr,null,2))
-    return 'update'
+        return 'delete'
+    }catch(err){
+        console.error('err',err.message)
+    }
+}
+
+async function readRiddle(path){
+    try{
+    const data=await readFile(path,'utf8');
+    console.log( data);
+
     }
     catch(err){
+        console.log('err',err.message);
+    }
+}
+
+
+async function creatAll(path,fn) {
+    try{
+    const res=await fn()
+    const data=await readFile(path,'utf8')
+    const jsData=JSON.parse(data)
+    let currentId;
+    jsData.length === 0 ? currentId = 1 : currentId = jsData[jsData.length-1].id + 1;
+    res.id = currentId;
+    jsData.push(res)
+
+    await writeFile(path,JSON.stringify(jsData,null,2))
+    return'create'
+    
+    }catch(err){
         console.log("error",err.message); 
     }
 }
 
-creatRiddel('{"name":"yoel","score":98}')
-  .then(result => console.log (result))
-  .catch(err => console.error(err));
+const val=await creatAll(pathPlayer,creatPlayer)
+console.log(val);
+
+
+// await Delete(path)
+
+// await Update(path)
+
+// await readRiddle(path);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
