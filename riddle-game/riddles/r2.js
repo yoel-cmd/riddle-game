1c20cc037f21d50e331012e36fb2dc7db6282dc0
yoel ider <yoeli5746@gmail.com>
2025-07-09 16:14:52 +0300

    finish startGame

--- riddle-game/riddles/r2.js
+++
@@ -1,6 +0,0 @@
-export default {
-    id: 2,
-    name: "Easy Math",
-    taskDescription: "What is 10 - 4?",
-    corectAnswer: '6'
-}
--- riddle-game/riddles/r6.js
+++
@@ -1,6 +0,0 @@
-export default {
-    id: 6,
-    name: "Hard Logic",
-    taskDescription: "I am an odd number. Take away one letter and I become even. What am I?",
-    corectAnswer: 'seven'
-}
